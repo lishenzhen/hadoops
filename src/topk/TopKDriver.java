87b0936cd7bb6a861af30ea70af0b06b0e44bf64
lishenzhen <lishenzhencn@126.com>
2017-08-14 00:52:55 +0800

    @update | 修改作业提交格式

--- src/topk/TopKDriver.java
+++ src/topk/TopKDriver.java
@@ -1,7 +1,11 @@
 package topk;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.SequenceFileInputFormat;
@@ -18,25 +22,36 @@ import topk.step.WordTopK;
 public class TopKDriver {
 	
 	public static Logger logger = LoggerFactory.getLogger(TopKDriver.class);
+//	private static String fs_prefix= "hdfs://node4:9000";
 
 	public static void main(String[] args) throws Exception{
 
-		Configuration conf = new Configuration();
-		conf.set("fs.defaultFS", "hdfs://node4:9000");
-		conf.set("N", "10");
+		if (args.length != 3) {
+			logger.warn("usage TopKDriver <N> <input> <output>");
+	         System.exit(1);
+	      }
+		int N = Integer.parseInt(args[0]);
+		String inputUrl = args[1];
+		String outputUrl = args[2];
 		
-	    if (submitStep1(conf)) {
-			logger.info("step 1 is ok.");
-			conf = new Configuration();
-			conf.set("fs.defaultFS", "hdfs://node4:9000");
-			conf.set("N", "10");
-			if (submitStep2(conf)) {
-				logger.debug("step 2 is ok.");
+		Configuration conf = new Configuration();
+//		conf.set("fs.defaultFS", fs_prefix);
+		conf.set("N", N+"");
+		FileSystem fs = FileSystem.get(conf);
+		Path inputPath = new Path(inputUrl);
+		Long taskId = System.currentTimeMillis();
+		Path tmpPath = new Path("/tmp/topk/"+taskId);
+		Path outputPath =new Path(outputUrl);
+	    if (submitStep1(conf,inputPath,tmpPath)) {
+			logger.info("TOPK PROGRESS: step 1 is OK.");
+			if (submitStep2(conf,tmpPath,outputPath)) {
+				fs.delete(tmpPath);
+				logger.debug("TOPK PROGRESS: step 2 is OK.");
 			}
 		}
 	}
 	
-	public static boolean submitStep1(Configuration conf)throws Exception{
+	public static boolean submitStep1(Configuration conf,Path inputPath,Path outPath)throws Exception{
 		
 		Job job =new Job(conf);
 		job.setJobName("topk-step1");
@@ -47,26 +62,41 @@ public class TopKDriver {
 		job.setMapperClass(WordCount.WordCountMapper.class);
 		job.setReducerClass(WordCount.WordCountReducer.class);
 		
-		FileInputFormat.setInputPaths(job, new Path("/data/input/wordcount"));
-	    FileOutputFormat.setOutputPath(job, new Path("/tmp/topk/wordcount"));
+		job.setMapOutputKeyClass(Text.class);
+		job.setMapOutputValueClass(IntWritable.class);
+		
+		job.setOutputKeyClass(Text.class);
+	   job.setOutputValueClass(IntWritable.class);
+		
+		FileInputFormat.setInputPaths(job, inputPath);
+	    FileOutputFormat.setOutputPath(job, outPath);
 	    
 	    return job.waitForCompletion(true);
 	}
 
-	public static boolean submitStep2(Configuration conf)throws Exception{
+	public static boolean submitStep2(Configuration conf,Path inputPath,Path outPath)throws Exception{
 		
 		Job job =new Job(conf);
 		job.setJobName("topk-step2");
 		
 		job.setInputFormatClass(SequenceFileInputFormat.class);
+//		job.setOutputFormatClass(SequenceFileOutputFormat.class);
 		job.setOutputFormatClass(TextOutputFormat.class);
 		
+		job.setMapOutputKeyClass(NullWritable.class);   
+	   job.setMapOutputValueClass(Text.class);   
+	      
+	      // reduce()'s output (K,V)
+	   job.setOutputKeyClass(IntWritable.class);
+	   job.setOutputValueClass(Text.class);
+	      
 		job.setMapperClass(WordTopK.WordTopKMapper.class);
 		job.setReducerClass(WordTopK.WordTopKRedeucer.class);
 		
-		FileInputFormat.setInputPaths(job, new Path("/tmp/topk/wordcount"));
-	    FileOutputFormat.setOutputPath(job, new Path("/tmp/topk/wordcount"));
+		FileInputFormat.setInputPaths(job, inputPath);
+	    FileOutputFormat.setOutputPath(job, outPath);
 	    
 	    return job.waitForCompletion(true);
 	}
+	
 }
